650cf1845602c03e54502dbc35a417bac3a598b4
Abhijeet <alondhe649@gmail.com>
2019-06-26 11:24:35 +0530

    sql

--- SQL/query1.sql
+++ SQL/query1.sql
@@ -17,3 +17,15 @@ select * from emp where job="clerk";
 select ename,hiredate,date_format(from_days(datediff(CURDATE(),hiredate)),'%Y' )+0 as experience from emp;
 --display employees unique dept no in emp
 select distinct deptno from emp;
+--display salary of scott
+select sal from emp where ename="scott"
+--display employee having same salary of scott
+select ename,sal from emp where sal=(select sal from emp where ename="scott"); 
+--display employee working in same dept of blake
+select ename,deptno from emp where deptno=(select deptno from emp where ename="blake");
+--display no of emp's
+select count(*) from emp
+-- avg of sal 
+select avg(sal) from emp
+--sum of sal
+select sum(sal) from emp
